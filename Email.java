683b25ea22d3e680125866099b542df762c0f2e6
AnushaManda-27 <anushamanda27@gmail.com>
2021-05-20 16:23:12 +0530

    Added: Validation of EmailId

--- Email.java
+++ Email.java
@@ -1,10 +1,22 @@
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Email {
-	
-	public static void main(String[] args) {
-	
-		System.out.println("Welcome");
+	public static boolean isValid(String email) {
+		
+		String emailid = "[0-9 a-z A-Z]{3}([.][0-9 a-z A-Z]{3}+[@])?[a-z]{10}([.])?[a-z]{2}([.][a-z]{2})?";
+		
+		Pattern p = Pattern.compile(emailid);
+		if(email == null) {
+			return false;
+		}
+		
+		Matcher m = p.matcher(email);
+		return m.matches();
 	}
-
+	
+	public static void main(String args[]) {
+	String email11 = "abcbridgelabz.co";
+	System.out.println(email11 +":" +isValid(email11));
+	}			
 }
